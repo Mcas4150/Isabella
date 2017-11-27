07024def6455de0eee283745b724b6984bef1d38
Michael Cassidy <mikecassidy0000@gmail.com>
2017-11-27 15:21:55 -0500

    pg

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 
-
-
+# config.assets.paths << Rails.root.join("app", "assets", "fonts")
+# config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
 module Isabella
   class Application < Rails::Application
